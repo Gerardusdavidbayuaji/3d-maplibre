968b6248705acb661e4a99f98c85ba7970684560
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-10-28 11:15:32 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const TOKEN = process.env.ACCESS_TOKEN;
+import { Tile3DLayer } from "@deck.gl/geo-layers";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center text-center w-full h-screen bg-gray-400">
+      Kolom Map
+    </div>
+  );
+};
+
+export default App;
